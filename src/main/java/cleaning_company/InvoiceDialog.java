c356014b0f54417938efd00f36bfbd3c4c8c0254
Noor Al-huda Hamayel <nooralhudahamayel7@gmail.com>
2023-05-13 04:38:04 +0300

    fix sonar bad smells

--- src/main/java/cleaning_company/InvoiceDialog.java
+++ src/main/java/cleaning_company/InvoiceDialog.java
@@ -6,7 +6,7 @@ import javax.swing.JOptionPane;
 
 public class InvoiceDialog extends javax.swing.JDialog {
 
-    Product p;
+	transient Product p;
     
     public InvoiceDialog(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
@@ -19,76 +19,79 @@ public class InvoiceDialog extends javax.swing.JDialog {
     
     private void initComponents() {
 
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
+    	javax.swing.JLabel jLabel2 = new javax.swing.JLabel();
+    	javax.swing.JLabel jLabel3 = new javax.swing.JLabel();
+    	javax.swing.JLabel jLabel4 = new javax.swing.JLabel();
+    	javax.swing.JLabel jLabel5 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
         jTextField2 = new javax.swing.JTextField();
         jTextField3 = new javax.swing.JTextField();
         jTextField4 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
+        javax.swing.JButton jButton1 = new javax.swing.JButton();
+        javax.swing.JLabel jLabel1 = new javax.swing.JLabel();
+        String segoeUI="Segoe UI";
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         addWindowListener(new java.awt.event.WindowAdapter() {
+        	
+        	@Override
             public void windowOpened(java.awt.event.WindowEvent evt) {
-                formWindowOpened(evt);
+                formWindowOpened();
             }
         });
 
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 18)); 
+        jLabel2.setFont(new java.awt.Font(segoeUI, 0, 18)); 
         jLabel2.setText("Items");
 
-        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 18)); 
+        jLabel3.setFont(new java.awt.Font(segoeUI, 0, 18)); 
         jLabel3.setText("Clean cost");
 
-        jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 18)); 
+        jLabel4.setFont(new java.awt.Font(segoeUI, 0, 18)); 
         jLabel4.setText("Delivery Cost");
         jLabel4.setToolTipText("");
 
-        jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 18)); 
+        jLabel5.setFont(new java.awt.Font(segoeUI, 0, 18)); 
         jLabel5.setText("Total Amount");
 
         jTextField1.setEditable(false);
-        jTextField1.setFont(new java.awt.Font("Segoe UI", 0, 18)); 
+        jTextField1.setFont(new java.awt.Font(segoeUI, 0, 18)); 
         jTextField1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
+                jTextField1ActionPerformed();
             }
         });
 
-        jTextField2.setFont(new java.awt.Font("Segoe UI", 0, 18)); 
+        jTextField2.setFont(new java.awt.Font(segoeUI, 0, 18)); 
         jTextField2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField2ActionPerformed(evt);
+                jTextField2ActionPerformed();
             }
         });
 
-        jTextField3.setFont(new java.awt.Font("Segoe UI", 0, 18)); 
+        jTextField3.setFont(new java.awt.Font(segoeUI, 0, 18)); 
         jTextField3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField3ActionPerformed(evt);
+                jTextField3ActionPerformed();
             }
         });
 
         jTextField4.setEditable(false);
-        jTextField4.setFont(new java.awt.Font("Segoe UI", 0, 18)); 
+        jTextField4.setFont(new java.awt.Font(segoeUI, 0, 18)); 
         jTextField4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField4ActionPerformed(evt);
+                jTextField4ActionPerformed();
             }
         });
 
-        jButton1.setFont(new java.awt.Font("Segoe UI", 0, 18)); 
+        jButton1.setFont(new java.awt.Font(segoeUI, 0, 18)); 
         jButton1.setText("Create");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                jButton1ActionPerformed();
             }
         });
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 24)); 
+        jLabel1.setFont(new java.awt.Font(segoeUI, 0, 24)); 
         jLabel1.setText("Invoice");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -142,23 +145,24 @@ public class InvoiceDialog extends javax.swing.JDialog {
         pack();
     }
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {
-       
-    }
-
-    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {
-
-    }
-
-    private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {
-
-    }
-
-    private void jTextField4ActionPerformed(java.awt.event.ActionEvent evt) {
+    private void jTextField1ActionPerformed() {
     	
+    	// This method is intentionally left empty
     }
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
+    private void jTextField2ActionPerformed() {
+    	// This method is intentionally left empty
+    }
+
+    private void jTextField3ActionPerformed() {
+    	// This method is intentionally left empty
+    }
+
+    private void jTextField4ActionPerformed() {
+    	// This method is intentionally left empty
+    }
+
+    private void jButton1ActionPerformed() {
 
         if (p != null) {
             float cleanCost = 0;
@@ -194,13 +198,13 @@ public class InvoiceDialog extends javax.swing.JDialog {
         }
     }
 
-    private void formWindowOpened(java.awt.event.WindowEvent evt) {
+    private void formWindowOpened() {
        
         jTextField1.setText(p.name);
     }
 
     
-    public static void main(String args[]) {
+    public static void main(String[] args) {
        
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -209,15 +213,12 @@ public class InvoiceDialog extends javax.swing.JDialog {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(InvoiceDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(InvoiceDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(InvoiceDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } 
+
+        catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(InvoiceDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+
 
 
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -235,12 +236,7 @@ public class InvoiceDialog extends javax.swing.JDialog {
     }
 
    
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
+    
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
